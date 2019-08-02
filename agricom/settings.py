8a65973f8f08217c617a1c67d319c3b53f35e570
Paal Pedersen <paal.pedersen@geodata.no>
2019-08-02 19:38:01 +0200

    update

--- agricom/settings.py
+++ agricom/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '9g80!d_un#x&jw=%h4##8*-lb0s4d0%fb1gt2qt-^$j5o+&u9k'
+SECRET_KEY = '9g80!d_un#x&jw=%h4##8*-lb0s4d0%fb1gt2qt-^$j5o+&u9k'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True
