ad4c3841745d5802cc2f62fe684520070f238297
Paal Pedersen <paalped@gmail.com>
2019-08-03 01:16:51 +0200

    update host

--- agricom/settings.py
+++ agricom/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
         'NAME': 'agricom',
         'USER': 'postgres',
-        'HOST': 'db',
+        'HOST': 'main',
         'PASSWORD': 'postgres',
         'PORT': 25432,
     }
